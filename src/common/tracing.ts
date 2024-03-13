5b5412846e3196b024590db665c6a3f31d3b898e
razbroc <razbroc@gmail.com>
2024-03-13 20:08:58 +0200

    chore: configure ignored routes

--- src/common/tracing.ts
+++ src/common/tracing.ts
@@ -1,12 +1,21 @@
 import { Tracing } from '@map-colonies/telemetry';
-import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { ExpressInstrumentation, ExpressLayerType  } from '@opentelemetry/instrumentation-express';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { IGNORED_INCOMING_TRACE_ROUTES, IGNORED_OUTGOING_TRACE_ROUTES } from './constants';
 
-export const tracing = new Tracing([
-  new HttpInstrumentation({
-    ignoreIncomingPaths: IGNORED_INCOMING_TRACE_ROUTES,
-    ignoreOutgoingUrls: IGNORED_OUTGOING_TRACE_ROUTES,
-  }),
-  new ExpressInstrumentation(),
-]);
+export const tracing = new Tracing(
+  [
+    new HttpInstrumentation({
+      ignoreIncomingPaths: IGNORED_INCOMING_TRACE_ROUTES,
+      ignoreOutgoingUrls: IGNORED_OUTGOING_TRACE_ROUTES,
+    }),
+    new ExpressInstrumentation(),
+  ],
+  {
+    /* eslint-disable @typescript-eslint/naming-convention */
+    '@opentelemetry/instrumentation-fs': { requireParentSpan: true },
+    '@opentelemetry/instrumentation-http': { requireParentforOutgoingSpans: true },
+    '@opentelemetry/instrumentation-express': { ignoreLayersType: [ExpressLayerType.MIDDLEWARE] },
+    /* eslint-disable @typescript-eslint/naming-convention */
+  }
+);
